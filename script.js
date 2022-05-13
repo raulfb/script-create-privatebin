a0aadf16a494a758fb5bcd44d94097c5eb9060b3
Raúl <43234547+raulfb@users.noreply.github.com>
2022-05-13 17:17:20 +0200

    options opts

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const key = crypto.webcrypto.getRandomValues(new Uint8Array(32))
 
 const msg = 'Hola mundo'
 
+// options opts
+// expire: '5min' | '10min' | '1hour' | '1day' | '1week' | '1month' | '1year' | 'never';
+// burnafterreading: 0 | 1;
+// opendiscussion: 0 | 1;
+// output: 'text' | 'json' | 'yaml';
+// compression: 'none' | 'zlib';
+// textformat: 'plaintext' | 'markdown';
+
 const opts = {
     textformat: 'plaintext',
     expire: '1week',
